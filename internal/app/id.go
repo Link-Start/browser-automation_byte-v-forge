d1d230a4532e542197b3481fa2d2ad04d2192b1b
pood1e <codingpood1e@gmail.com>
2026-05-25 00:46:18 +0800

    refactor browser id fallback

--- internal/app/id.go
+++ internal/app/id.go
@@ -2,15 +2,22 @@ package app
 
 import (
 	"crypto/rand"
+	"encoding/binary"
 	"encoding/hex"
+	"sync/atomic"
+	"time"
 )
 
 type RandomIDGenerator struct{}
 
+var fallbackIDCounter atomic.Uint64
+
 func (RandomIDGenerator) NewID(prefix string) string {
 	var buf [16]byte
-	if _, err := rand.Read(buf[:]); err != nil {
-		panic(err)
+	if _, err := rand.Read(buf[:]); err == nil {
+		return prefix + hex.EncodeToString(buf[:])
 	}
+	binary.BigEndian.PutUint64(buf[:8], uint64(time.Now().UnixNano()))
+	binary.BigEndian.PutUint64(buf[8:], fallbackIDCounter.Add(1))
 	return prefix + hex.EncodeToString(buf[:])
 }
